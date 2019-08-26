e2cc6221d1441f620da9e95d9a70898bd8c78390
Rotem <rotem.kincaid@nikolamotor.com>
2019-08-26 10:30:53 -0700

    added custom route for toggle status

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
 
   root to: 'pages#home'
 end
