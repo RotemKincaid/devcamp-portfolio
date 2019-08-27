fb52eb9bd0a4a161f6650a8b8b37b86dbe5eb196
Rotem <rotem.kincaid@nikolamotor.com>
2019-08-27 11:28:20 -0700

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'react-items', to: 'portfolios#react'
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
 
   get 'about-me', to: 'pages#about'
 
