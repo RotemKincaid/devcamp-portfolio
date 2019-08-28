69faaa8d04bef690934cf0d35fa23fd7d0a75b36
Rotem <rotem.kincaid@nikolamotor.com>
2019-08-28 13:16:21 -0700

    Added nav bar and added default header content

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -4,12 +4,12 @@ module CurrentUserConcern
     def current_user
         super || guest_user
     end
-    
+
     def guest_user
        OpenStruct.new(name: "Guest User",
                       first_name: "Guest",
                       last_name: "User", 
                       email: "Guest@user.com"
                       )
-    end
+    end 
 end
